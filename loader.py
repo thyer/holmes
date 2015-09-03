050a03b17bd1e5a667d5deaccc3e2d1d803bbc34
Trenton Adam Hyer <thyer@ta-7.cs.byu.edu>
2015-09-02 19:11:33 -0600

    KWIC working correctly

--- loader.py
+++ loader.py
@@ -13,37 +13,56 @@ class Corpus:
     def load(self, path):
         with open(path) as text_file:
             for line in text_file:
-                self.prev = None
+                self.prev_word = None
                 self.current = None
                 for word in line.split():
-                    if not self.dictionary.__contains__(word):
+                    word_cleaned = re.sub(r'\W+', '', word.upper())
+                    if not self.dictionary.__contains__(word_cleaned):
                         self.current = Node(word)
+                        self.current.prev = self.prev_word
                         value = []
-                        self.dictionary[re.sub(r'\W+', '', word.upper())] = value
+                        self.dictionary[word_cleaned] = value
                         value.append(self.current)
                     else:
                         self.current = Node(word)
-                        self.dictionary[word.upper()].append(self.current)
+                        self.current.prev = self.prev_word
+                        self.dictionary[word_cleaned].append(self.current)
 
-                    if self.prev:
-                        self.prev.next = self.current
-                        self.prev = self.current
+                    if self.prev_word:
+                        self.prev_word.next = self.current
+                    self.prev_word = self.current
 
     def search(self, token):
-        output = []
-        output.append("Search Results: ")
-        linked_list = self.dictionary[token.upper()]
-        if not linked_list:
-            output.append("\tWORD NOT FOUND")
+        output = "Search Results: "
+        token = re.sub(r'\W+', '', token.upper())
+        if not self.dictionary.__contains__(token):
+            output += ("\n\tWORD NOT FOUND")
             return output
+        linked_list = self.dictionary[re.sub(r'\W+', '', token.upper())]
         for item in linked_list:
-            print(item.next)
-            output.append(self.get_KWIC(item))
+            output += "\n\t" + (self.get_KWIC(item))
         return output
 
     def get_KWIC(self, item):
         output = ""
+        temp = item
+        
+        # Add up to five words before key word
+        for i in range(4):
+            if temp.prev:
+                output = temp.prev.text + " " + output
+                temp = temp.prev
+                
+        # Add key word
         output += item.text.upper()
+        
+        # Add up to five words following key word
+        for i in range(4):
+            if item.next:
+                output += " " + item.next.text
+                item = item.next
+
+        return output
 
 
 class Node:
@@ -53,7 +72,11 @@ class Node:
         self.next = None
 
 
-count = 0
+print("Welcome to HOLMES")
 corpus = Corpus("holmes")
-print(corpus.search("AMUSING"))
-print(str(corpus.dictionary))
+input = ""
+while not input == "QUIT":
+    if not input == "":
+        print(corpus.search(input) + "\n\n\n\n\n")
+    print("Enter a search term. Type \'QUIT\' to end the program")
+    input = raw_input('--> ')
